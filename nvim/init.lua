f4734e012d7ba99eb6708d459e18f466ad520ae2
laacin <lautarocingolani07@gmail.com>
2025-05-07 21:29:17 -0300

    feat: add ui plugins

--- nvim/init.lua
+++ nvim/init.lua
@@ -12,4 +12,22 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("options")
-require("lazy").setup("plugins")
+require("lazy").setup(
+  "plugins",
+  {
+  spec = {
+    { import = "lazyvim.plugins.extras.formatting.biome" },
+    { import = "lazyvim.plugins.extras.formatting.prettier" },
+
+    -- Linting plugins
+    { import = "lazyvim.plugins.extras.linting.eslint" },
+
+    -- Language support plugins
+    { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.markdown" },
+    { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.lang.go" },
+    { import = "lazyvim.plugins.extras.lang.toml" },
+    { import = "lazyvim.plugins.extras.lang.rust" },
+  },
+})
