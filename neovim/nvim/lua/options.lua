ef3b0af59115004d42f61b69f6da52bcb34ff2c8
laacin <lautarocingolani07@gmail.com>
2025-05-09 01:01:29 -0300

    refactor: use kickstart-nvim as base config

---
+++ neovim/nvim/lua/options.lua
@@ -0,0 +1,26 @@
+vim.cmd 'set expandtab'
+vim.cmd 'set tabstop=2'
+vim.cmd 'set softtabstop=2'
+vim.cmd 'set shiftwidth=2'
+
+vim.opt.swapfile = false
+
+-- Navigate vim panes better
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+vim.wo.number = true
+
+-- Hidde vim bar
+vim.opt.cmdheight = 0
+
+-- Formatting
+vim.api.nvim_create_augroup('FormatOnSave', { clear = true })
+
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.g.have_nerd_font = true
--- old-nvim/lua/lsp/setup.lua
+++
@@ -1,5 +0,0 @@
--- LSP
-vim.lsp.enable("lua_ls")
-vim.lsp.enable("vtsls")
-
-return {}
