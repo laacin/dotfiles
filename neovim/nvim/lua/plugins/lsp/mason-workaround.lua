23b944decc9e0f0633208ceb74a68be51f02d96f
laacin <lautarocingolani07@gmail.com>
2025-05-10 03:08:08 -0300

    fix: lsp config issue fixed

--- neovim/nvim/lua/plugins/lsp/mason-workaround.lua
+++
@@ -1,7 +0,0 @@
-return {
-  { "williamboman/mason.nvim", enabled = false },
-  { "williamboman/mason-lspconfig.nvim", enabled = false },
-  { "mason-org/mason.nvim" },
-  { "mason-org/mason-lspconfig.nvim" },
-  { "neovim/nvim-lspconfig" },
-}
