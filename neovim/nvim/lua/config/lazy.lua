8b02d7f01ee96358058546821b3bcf2bca39c47f
laacin <lautarocingolani07@gmail.com>
2025-05-09 21:09:52 -0300

    fix: temp disable mason

--- neovim/nvim/lua/config/lazy.lua
+++ neovim/nvim/lua/config/lazy.lua
@@ -54,9 +54,9 @@ require("lazy").setup({
     rtp = {
       disabled_plugins = {
         "gzip",
-        -- "matchit",
-        -- "matchparen",
-        -- "netrwPlugin",
+        "matchit",
+        "matchparen",
+        "netrwPlugin",
         "tarPlugin",
         "tohtml",
         "tutor",
---
+++ neovim/nvim/lua/plugins/mason-workaround.lua
@@ -0,0 +1,7 @@
+return {
+  { "williamboman/mason.nvim", enabled = false },
+  { "williamboman/mason-lspconfig.nvim", enabled = false },
+  { "mason-org/mason.nvim" },
+  { "mason-org/mason-lspconfig.nvim" },
+  { "neovim/nvim-lspconfig" },
+}
