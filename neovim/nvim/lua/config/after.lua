1b625527fe865097b0406ab1a627be5e072b8cba
laacin <lautarocingolani07@gmail.com>
2025-05-10 23:34:21 -0300

    feat(nvim): add enable/disable transparency command v2

--- neovim/nvim/lua/config/after.lua
+++ neovim/nvim/lua/config/after.lua
@@ -3,19 +3,12 @@ vim.cmd.colorscheme("catppuccin")
 
 -- Disable transparency
 vim.api.nvim_create_user_command("DisableTransparency", function()
-  require("catppuccin").setup({
-    flavour = "mocha",
-    transparent_background = false,
-    term_colors = true,
-  })
-  vim.cmd.colorscheme("catppuccin")
+  vim.api.nvim_set_hl(0, "Normal", { bg = "#030303" })
+  vim.api.nvim_set_hl(0, "NormalNC", { bg = "#030303" })
+  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#030303" })
+  vim.api.nvim_set_hl(0, "FloatBorder", { bg = "#030303" })
 end, {})
 
 vim.api.nvim_create_user_command("EnableTransparency", function()
-  require("catppuccin").setup({
-    flavour = "mocha",
-    transparent_background = true,
-    term_colors = true,
-  })
   vim.cmd.colorscheme("catppuccin")
 end, {})
