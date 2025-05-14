a3962bb29a6b82385ba12ccbf98d8191759101bd
laacin <lautarocingolani07@gmail.com>
2025-05-14 06:04:17 -0300

    feat(nvim): highlight line color number

--- neovim/nvim/lua/config/autocmds.lua
+++ neovim/nvim/lua/config/autocmds.lua
@@ -1,2 +1,4 @@
 -- Cursorline
-vim.opt.cursorline = false
+vim.opt.cursorline = true
+vim.api.nvim_set_hl(0, "CursorLineNr", { bold = true, fg = "#D7AF5F" })
+vim.api.nvim_set_hl(0, "CursorLine", { bg = "NONE", ctermbg = "NONE" })
