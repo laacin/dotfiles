5a72d258a10e77318be914d934be48015e2661f2
laacin <lautarocingolani07@gmail.com>
2025-05-15 23:17:07 -0300

    fix(nvim): fix bug where enabling transparency causes the cursorline to appear

--- neovim/nvim/lua/config/after.lua
+++ neovim/nvim/lua/config/after.lua
@@ -9,4 +9,6 @@ end, {})
 
 vim.api.nvim_create_user_command("EnableBackground", function()
   vim.cmd.colorscheme(colorscheme)
+  vim.api.nvim_set_hl(0, "CursorLineNr", { bold = true, fg = "#D7AF5F" })
+  vim.api.nvim_set_hl(0, "CursorLine", { bg = "NONE", ctermbg = "NONE" })
 end, {})
--- neovim/nvim/lua/config/autocmds.lua
+++ neovim/nvim/lua/config/autocmds.lua
@@ -1,4 +1,3 @@
 -- Cursorline
-vim.opt.cursorline = true
 vim.api.nvim_set_hl(0, "CursorLineNr", { bold = true, fg = "#D7AF5F" })
 vim.api.nvim_set_hl(0, "CursorLine", { bg = "NONE", ctermbg = "NONE" })
