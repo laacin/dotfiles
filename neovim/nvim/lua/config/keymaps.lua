bb3ae837f1f68f02b3a2bc50c4cd3678c52a095c
laacin <lautarocingolani07@gmail.com>
2025-09-09 15:39:42 -0300

    chore: add keymap to show ALL LSP logs for a buffer

--- neovim/nvim/lua/config/keymaps.lua
+++ neovim/nvim/lua/config/keymaps.lua
@@ -8,3 +8,7 @@ vim.keymap.set("n", "<leader>e", "<cmd>Neotree toggle<CR>", { desc = "Toggle Neo
 vim.keymap.set("n", "<leader>de", function()
   vim.diagnostic.open_float(nil, { border = "rounded" })
 end, { noremap = true, silent = true, desc = "LSP logs" })
+
+vim.keymap.set("n", "<leader>dE", function()
+  vim.diagnostic.setloclist({ open = true, border = "rounded" })
+end, { noremap = true, silent = true, desc = "LSP logs (ALL)" })
