540bb3f3bd4d130b412e12be41bb1654e19e7416
laacin <lautarocingolani07@gmail.com>
2025-09-09 13:49:03 -0300

    feat: add keymap to open LSP logs

--- neovim/nvim/lua/config/keymaps.lua
+++ neovim/nvim/lua/config/keymaps.lua
@@ -3,3 +3,8 @@ vim.keymap.set("n", "-", "<cmd>Oil<CR>")
 
 -- Neotree
 vim.keymap.set("n", "<leader>e", "<cmd>Neotree toggle<CR>", { desc = "Toggle Neo-tree" })
+
+-- LSP logs
+vim.keymap.set("n", "<leader>de", function()
+  vim.diagnostic.open_float(nil, { border = "rounded" })
+end, { noremap = true, silent = true, desc = "LSP logs" })
