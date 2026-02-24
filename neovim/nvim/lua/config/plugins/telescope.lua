7335b17d94f6e7f6b05bd210569966c683a4fb14
laacin <lautarocingolani07@gmail.com>
2026-02-24 11:20:25 -0300

    feat(telescope): ignore `.json` and `.lock` files

--- neovim/nvim/lua/config/plugins/telescope.lua
+++ neovim/nvim/lua/config/plugins/telescope.lua
@@ -1,6 +1,6 @@
 require("telescope").setup({
   defaults = {
-    file_ignore_patterns = { "node_modules", "%.git/" },
+    file_ignore_patterns = { "node_modules", "%.lock", "%.json", "%.git/" },
     file_previewer = require("telescope.previewers").vim_buffer_cat.new,
     mappings = {
       i = { ["<C-h>"] = "which_key" },
