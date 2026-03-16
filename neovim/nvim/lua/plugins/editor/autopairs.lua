726efbc273a3034e071e181cfa39fb082aab209d
laacin <lautarocingolani07@gmail.com>
2026-03-15 22:41:16 -0300

    fix(nvim): temp fix for autopairs split

--- neovim/nvim/lua/plugins/editor/autopairs.lua
+++ neovim/nvim/lua/plugins/editor/autopairs.lua
@@ -1,8 +1,29 @@
+-- temporal fix
+local function cfg(_, opts)
+  require("nvim-autopairs").setup(opts)
+
+  vim.keymap.set("i", "<CR>", function()
+    local col = vim.fn.col(".")
+    local line = vim.fn.getline(".")
+    local prev = line:sub(col - 1, col - 1)
+    local next = line:sub(col, col)
+
+    local pairs = { ["("] = ")", ["{"] = "}", ["["] = "]", ['"'] = '"', ["'"] = "'" }
+
+    if pairs[prev] == next then
+      return "\n<Esc>O"
+    end
+
+    return "\n"
+  end, { expr = true })
+end
+
 return {
   {
     "windwp/nvim-autopairs",
     event = "InsertEnter",
-    opts = {},
+    opts = { map_cr = false },
+    config = cfg,
   },
   {
     "windwp/nvim-ts-autotag",
