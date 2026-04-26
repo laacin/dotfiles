43827ab5279540dd32df57ad17fce4dba0366397
laacin <lautarocingolani07@gmail.com>
2026-04-26 18:02:23 -0300

    refactor: remove temporary autopairs workaround and restore default configuration

--- neovim/nvim/lua/plugins/editor/autopairs.lua
+++ neovim/nvim/lua/plugins/editor/autopairs.lua
@@ -1,29 +1,31 @@
 -- temporal fix
-local function cfg(_, opts)
-  require("nvim-autopairs").setup(opts)
-
-  vim.keymap.set("i", "<CR>", function()
-    local col = vim.fn.col(".")
-    local line = vim.fn.getline(".")
-    local prev = line:sub(col - 1, col - 1)
-    local next = line:sub(col, col)
-
-    local pairs = { ["("] = ")", ["{"] = "}", ["["] = "]", ['"'] = '"', ["'"] = "'" }
-
-    if pairs[prev] == next then
-      return "\n<Esc>O"
-    end
-
-    return "\n"
-  end, { expr = true })
-end
+-- local function cfg(_, opts)
+--   require("nvim-autopairs").setup(opts)
+--
+--   vim.keymap.set("i", "<CR>", function()
+--     local col = vim.fn.col(".")
+--     local line = vim.fn.getline(".")
+--     local prev = line:sub(col - 1, col - 1)
+--     local next = line:sub(col, col)
+--
+--     local pairs = { ["("] = ")", ["{"] = "}", ["["] = "]", ['"'] = '"', ["'"] = "'" }
+--
+--     if pairs[prev] == next then
+--       return "\n<Esc>O"
+--     end
+--
+--     return "\n"
+--   end, { expr = true })
+-- end
 
 return {
   {
     "windwp/nvim-autopairs",
     event = "InsertEnter",
-    opts = { map_cr = false },
-    config = cfg,
+    -- opts = { map_cr = false },
+    -- config = cfg,
+    config = true,
+    opts = {},
   },
   {
     "windwp/nvim-ts-autotag",
